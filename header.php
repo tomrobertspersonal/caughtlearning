62c1bf49b6e4f93ba353e74df9f9da12581b0860
tomrobertspersonal <to_mroberts@hotmail.com>
2015-08-27 23:55:40 +0100

    Added some styles

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <!doctype html>
 <html <?php language_attributes(); ?> class="no-js">
-	<head>
+  <head>
       <meta charset="<?php bloginfo('charset'); ?>">    	
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
     	<meta name="viewport" content="width=device-width, initial-scale=1">
